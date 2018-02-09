fc527f5237921953a99bdfb5ebf79f0ebb096c51
Peter554 <byfield554@gmail.com>
2018-02-09 15:43:08 +0000

    ok

--- modules/slide_sampler.py
+++ modules/slide_sampler.py
@@ -3,90 +3,87 @@ import os
 import numpy as np
 from skimage import filters, color
 from skimage.morphology import disk
-from skimage.morphology import opening, dilation
-from PIL import Image
-
-
-# def pil_show(x):
-#     x = x.astype(np.float32)
-#     if np.max(x) < 1.01: x *= 255.
-#     x_pil = Image.fromarray(x)
-#     x_pil.show()
+from skimage.morphology import opening
 
 
 class Slide_Sampler(object):
 
-    def __init__(self, wsi_file, desired_down_sampling, size, background_desired_down_sampling=32):
+    def __init__(self, wsi_file, desired_downsampling, size):
         super(Slide_Sampler, self).__init__()
         self.wsi_file = wsi_file
         self.wsi = openslide.OpenSlide(self.wsi_file)
-        self.desired_down_sampling = desired_down_sampling
+        self.desired_downsampling = desired_downsampling
         self.size = size
-        self.background_desired_down_sampling = background_desired_down_sampling
-        self.get_level_and_downsampling()
-        self.width_available = int(self.wsi.dimensions[0] - self.down_sampling * size)
-        self.height_available = int(self.wsi.dimensions[1] - self.down_sampling * size)
-        self.get_background_mask()
+        self.level, self.downsampling = self.get_level_and_downsampling(desired_downsampling, 0.1)
+        self.width_available = int(self.wsi.dimensions[0] - self.downsampling * size)
+        self.height_available = int(self.wsi.dimensions[1] - self.downsampling * size)
+        self.background_mask = None
         print('\nInitialized Slide_Sampler for slide {}'.format(os.path.basename(self.wsi_file)))
-        print('Patches will be sampled at (level {0} == downsampling of {1}) with size {2} x {2}'.format(self.level,
-                                                                                                         self.down_sampling,
+        print('Patches will be sampled at level {0} == downsampling of {1}, with size {2} x {2}.'.format(self.level,
+                                                                                                         self.downsampling,
                                                                                                          self.size))
 
-    def get_level_and_downsampling(self):
+    def get_level_and_downsampling(self, desired_downsampling, threshold):
         """
-        Get the level and downsampling for the desired downsampling. If level is not found an exception is raised. Small threshold to allow for not exactly equal desired and true downsampling.
+        Get the level and downsampling for a desired downsampling.
+        A threshold is used to allow for not exactly equal desired and true downsampling.
+        If an appropriate level is not found an exception is raised.
         :return:
         """
-        threshold = 0.1
-        diffs = [abs(self.desired_down_sampling - self.wsi.level_downsamples[i]) for i in
+        diffs = [abs(desired_downsampling - self.wsi.level_downsamples[i]) for i in
                  range(len(self.wsi.level_downsamples))]
         minimum = min(diffs)
         if minimum > threshold:
             raise Exception(
                 '\nLevel not found for desired downsampling.\nAvailable downsampling factors are\n{}'.format(
                     self.wsi.level_downsamples))
-        self.level = diffs.index(minimum)
-        self.down_sampling = self.wsi.level_downsamples[self.level]
+        level = diffs.index(minimum)
+        return level, self.wsi.level_downsamples[level]
 
-    def get_background_mask(self, disk_radius=10):
+    def add_background_mask(self, desired_downsampling=32, threshold=4, disk_radius=10):
         """
-        Get a background mask. That is a binary, downsampled image where True denotes a tissue region. This is achieved by otsu thresholding on the saturation channel followed by morphological opening to remove noise.
+        Add a background mask. That is a binary, downsampled image where True denotes a tissue region.
+        This is achieved by otsu thresholding on the saturation channel followed by morphological opening to remove noise.
+        The mask desired downsampling factor has a default of 32. For a WSI captured at 40X this corresponds to 1.25X.
+        :param desired_downsampling:
+        :param threshold:
         :param disk_radius:
         :return:
         """
         print('\nGetting background mask...')
-        self.get_background_mask_level()
+        self.background_mask_level, self.background_mask_downsampling = self.get_level_and_downsampling(
+            desired_downsampling, threshold)
         low_res = self.wsi.read_region(location=(0, 0), level=self.background_mask_level,
                                        size=self.wsi.level_dimensions[self.background_mask_level]).convert('RGB')
         low_res_numpy = np.asarray(low_res)
         low_res_numpy_hsv = color.convert_colorspace(low_res_numpy, 'RGB', 'HSV')
         saturation = low_res_numpy_hsv[:, :, 1]
         value = filters.threshold_otsu(saturation)
-        mask = saturation > value
+        mask = (saturation > value)
         selem = disk(disk_radius)
         mask = opening(mask, selem)
-        self.background_mask = mask
-
-    def get_background_mask_level(self):
-        """
-        Get level and downsampling of background mask for the desired background downsampling. If level is not found an exception is raised. Medium threshold to allow for not exactly equal desired and true downsampling.
-        :return:
-        """
-        threshold = 4.
-        diffs = [abs(self.background_desired_down_sampling - self.wsi.level_downsamples[i]) for i in
-                 range(len(self.wsi.level_downsamples))]
-        minimum = min(diffs)
-        if minimum > threshold:
-            raise Exception(
-                '\nLevel not found for desired downsampling.\nAvailable downsampling factors are\n{}'.format(
-                    self.wsi.level_downsamples))
-        self.background_mask_level = diffs.index(minimum)
-        self.background_mask_down_sampling = self.wsi.level_downsamples[self.background_mask_level]
+        self.background_mask = mask.astype(np.uint8)
+        self.size_at_background_level = self.level_converter(self.size, self.level, self.background_mask_level)
 
     def get_patch(self):
-        w = np.random.choice(self.width_available)
-        h = np.random.choice(self.height_available)
-        patch = self.wsi.read_region(location=(w, h), level=self.level, size=(self.size, self.size)).convert('RGB')
+        """
+        Get a random patch from the WSI
+        :return:
+        """
+        done = 0
+        while not done:
+            w = np.random.choice(self.width_available)
+            h = np.random.choice(self.height_available)
+            patch = self.wsi.read_region(location=(w, h), level=self.level, size=(self.size, self.size)).convert('RGB')
+            if self.background_mask==None:
+                done=1
+            else:
+                i = self.level_converter(h, 0, self.background_mask_level)
+                j = self.level_converter(w, 0, self.background_mask_level)
+                delta = self.size_at_background_level
+                background_mask_patch = self.background_mask[i:i + delta, j:j + delta]
+                area = background_mask_patch.shape[0] * background_mask_patch.shape[1]
+                if np.sum(background_mask_patch) / area > 0.9: done = 1
         return patch
 
     def print_slide_properties(self):
@@ -101,6 +98,16 @@ class Slide_Sampler(object):
         print(self.wsi.level_count)
         print('with downsampling factors:')
         print(self.wsi.level_downsamples)
+
+    def level_converter(self, x, lvl_in, lvl_out):
+        """
+        Convert a coordinate 'x' from lvl_in to lvl_out
+        :param x:
+        :param lvl_in:
+        :param lvl_out:
+        :return:
+        """
+        return np.floor(x * self.wsi.level_downsamples[lvl_in] / self.wsi.level_downsamples[lvl_out]).astype(np.uint32)
 
 
 ###
