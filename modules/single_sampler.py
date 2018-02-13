49597a012bab4bc95a861bc7770d97838c8c7308
Peter554 <byfield554@gmail.com>
2018-02-13 14:16:43 +0000

    better

--- modules/single_sampler.py
+++ modules/single_sampler.py
@@ -14,7 +14,7 @@ from modules import utils
 
 class Single_Sampler(object):
 
-    def __init__(self, wsi_file, background_dir, annotation_dir=None, level0=40.):
+    def __init__(self, wsi_file, background_dir, annotation_dir, level0=40.):
         self.wsi_file = wsi_file
         self.background_dir = background_dir
         self.annotation_dir = annotation_dir
@@ -27,8 +27,7 @@ class Single_Sampler(object):
         if not truth:
             print('Background object not found. Generating now.')
             down = int(self.level0 / 1.25)
-            self.background = NumpyBackround(self.wsi, approx_downsampling=down, threshold=15)
-            print('Made background at {}X'.format(self.level0 / self.wsi.level_downsamples[self.background.level]))
+            self.background = NumpyBackround(self.wsi, approx_downsampling=down, threshold=20)
             os.makedirs(self.background_dir, exist_ok=1)
             self.pickle_NumpyBackground(savedir=self.background_dir)
         if truth:
@@ -37,89 +36,64 @@ class Single_Sampler(object):
             self.validate_NumpyBackground()
             pickling_off.close()
 
-        if annotation_dir is not None:
+        if annotation_dir == None:
+            self.annotation = None
+        else:
             truth, string = utils.string_in_directory(self.fileID, self.annotation_dir)
             if truth:
                 self.annotation = openslide.OpenSlide(string)
             if not truth:
                 self.annotation = None
-        else:
-            self.annotation = None
 
     def prepare_sampling(self, desired_downsampling, patchsize):
         self.desired_downsampling = desired_downsampling
         self.patchsize = patchsize
         self.level = utils.get_level(self.wsi, self.desired_downsampling, threshold=0.01)
+
         background_patchsize = self.level_converter(self.patchsize, self.level, self.background.level)
         self.background.add_patchsize(background_patchsize)
+
         if self.annotation is not None:
             self.annotation_level = utils.get_level(self.annotation, self.desired_downsampling, threshold=0.01)
-        self.get_seeds()
+
+        self.get_classes_and_seeds()
+
         self.width_available = int(self.wsi.dimensions[0] - self.wsi.level_downsamples[self.level] * self.patchsize)
         self.height_available = int(self.wsi.dimensions[1] - self.wsi.level_downsamples[self.level] * self.patchsize)
 
-    def get_seeds(self):
+    def get_classes_and_seeds(self):
+        mask = self.background.data
+        nonzero = np.nonzero(mask)
+        factor = self.wsi.level_downsamples[self.background.level]
+        N = nonzero[0].shape[0]
+        coordinates = [(int(nonzero[0][i] * factor), int(nonzero[1][i] * factor)) for i in range(N)]
+        shuffle(coordinates)
+        self.class_list = [0]
+        self.class_seeds = [coordinates]
+
         if self.annotation is None:
-            self.class_seeds = None
-            self.classes = None
-        else:
-            down = int(self.level0 / 2.5)  # annotation must have same level 0 as wsi
-            temp_level = utils.get_level(self.annotation, desired_downsampling=down, threshold=15)
-            downsample = self.annotation.level_downsamples[temp_level]
-            low_res = self.annotation.read_region((0, 0), temp_level,
-                                                  self.annotation.level_dimensions[temp_level]).convert('L')
-            low_res = np.asarray(low_res).copy()
-            classes = sorted(list(np.unique(low_res)))
-            assert classes[0] == 0
-            classes = classes[1:]
-            class_seeds = []
-            for c in classes:
-                mask = (low_res == c)
-                nonzero = np.nonzero(mask)
-                coordinates = [(int(nonzero[0][i] * downsample), int(nonzero[1][i] * downsample)) for i in
-                               range(nonzero[0].shape[0])]
-                shuffle(coordinates)  # inplace
-                class_seeds.append(coordinates)
-            self.class_seeds = class_seeds
-            self.classes = classes
+            return
 
-    def get_random_patch(self):
-        done = 0
-        while not done:
-            w = np.random.choice(self.width_available)
-            h = np.random.choice(self.height_available)
-            patch = self.wsi.read_region((w, h), self.level, (self.patchsize, self.patchsize))
-            patch = patch.convert('RGB')
-            i = self.level_converter(h, 0, self.background.level)
-            j = self.level_converter(w, 0, self.background.level)
-            background_patch = self.background.data[i:i + self.background.patchsize, j:j + self.background.patchsize]
-            background_patch = background_patch.astype(int)
-            if np.sum(background_patch) / (self.background.patchsize ** 2) > 0.9:
-                done = 1
-        info = {'w': w, 'h': h, 'parent': self.wsi_file, 'level': self.level, 'size': self.patchsize}
-        return patch, info
+        down = int(self.level0 / 2.5)  # annotation should have same resolution at level 0 as WSI
+        level = utils.get_level(self.annotation, desired_downsampling=down, threshold=20)
+        annotation_low_res = self.annotation.read_region((0, 0), level, self.annotation.level_dimensions[level])
+        annotation_low_res = annotation_low_res.convert('L')
+        annotation_low_res = np.asarray(annotation_low_res).copy()
+        classes = sorted(list(np.unique(annotation_low_res)))
+        assert classes[0] == 0
+        classes = classes[1:]
+        for c in classes:
+            mask = (annotation_low_res == c)
+            nonzero = np.nonzero(mask)
+            factor = self.annotation.level_downsamples[level]
+            N = nonzero[0].shape[0]
+            coordinates = [(int(nonzero[0][i] * factor), int(nonzero[1][i] * factor)) for i in range(N)]
+            shuffle(coordinates)
+            self.class_list.append(c)
+            self.class_seeds.append(coordinates)
 
-    def get_class0_patch(self):
-        if self.annotation is None:
-            patch, info = self.get_random_patch()
-            info['class'] = 0
-            return patch, info
-        done = 0
-        while not done:
-            patch, info = self.get_random_patch()
-            w, h = info['w'], info['h']
-            annotation_patch = self.annotation.read_region((w, h), self.annotation_level,
-                                                           (self.patchsize, self.patchsize))
-            annotation_patch = annotation_patch.convert('L')
-            annotation_patch = np.asarray(annotation_patch).copy()
-            annotation_patch = (annotation_patch > 0).astype(int)
-            if np.sum(annotation_patch) / (self.patchsize ** 2) < 0.1:
-                info['class'] = 0
-                done = 1
-        return patch, info
-
-    def try_patch_from_seed(self, seed, seedclass):
-        h, w = seed
+    def class_c_patch_i(self, c, i):
+        h, w = self.class_seeds[c][i]
         patch = self.wsi.read_region((w, h), self.level, (self.patchsize, self.patchsize))
         patch = patch.convert('RGB')
 
@@ -127,14 +101,14 @@ class Single_Sampler(object):
         j = self.level_converter(w, 0, self.background.level)
         background_patch = self.background.data[i:i + self.background.patchsize, j:j + self.background.patchsize]
         background_patch = background_patch.astype(int)
-        if np.sum(background_patch) / (self.background.patchsize ** 2) < 0.9:
+        if not np.sum(background_patch) / (self.background.patchsize ** 2) > 0.9:
             # print('Patch rejected. Too much background.')
             return None, None
 
         annotation_patch = self.annotation.read_region((w, h), self.annotation_level, (self.patchsize, self.patchsize))
         annotation_patch = annotation_patch.convert('L')
         annotation_patch = np.asarray(annotation_patch).copy()
-        mask = (annotation_patch != seedclass).astype(int)
+        mask = (annotation_patch != c).astype(int)
         if np.sum(mask) / (self.patchsize ** 2) > 0.9:
             # print('Patch rejected. Too much of other classes.')
             return None, None
@@ -145,28 +119,23 @@ class Single_Sampler(object):
             'parent': self.wsi_file,
             'size': self.patchsize,
             'level': self.level,
-            'class': seedclass
+            'class': c
         }
         return patch, info
 
     def sample_patches(self, max_per_class=100, savedir=os.getcwd()):
         frame = pd.DataFrame(data=None, columns=['w', 'h', 'class', 'parent', 'level', 'size'])
-        # get class0 patches
-        for i in range(max_per_class):
-            _, info = self.get_class0_patch()
-            frame = frame.append(info, ignore_index=1)
 
-        # get other patches, if they exist
-        if self.annotation is not None and self.classes is not None:
-            for i, c in enumerate(self.classes):
-                seeds = self.class_seeds[i]
-                for j, seed in enumerate(seeds):
-                    _, info = self.try_patch_from_seed(seed, c)
-                    if info is not None:
-                        frame = frame.append(info, ignore_index=1)
-                    if j >= (max_per_class - 1):
-                        break
+        for i, c in enumerate(self.class_list):
+            seeds = self.class_seeds[i]
+            for j, seed in enumerate(seeds):
+                _, info = self.class_c_patch_i(c, j)
+                if info is not None:
+                    frame = frame.append(info, ignore_index=1)
+                if j >= (max_per_class - 1):
+                    break
 
+        os.makedirs(savedir, exist_ok=1)
         filename = os.path.join(savedir, self.fileID + '_patchframe.pickle')
         print('Saving patchframe to {}'.format(filename))
         frame.to_pickle(filename)
@@ -194,9 +163,44 @@ class Single_Sampler(object):
 
 class NumpyBackround(object):
 
-    def __init__(self, parent_wsi, approx_downsampling, threshold=15):
+    def __init__(self, parent_wsi, approx_downsampling, threshold=20):
         self.level = utils.get_level(parent_wsi, desired_downsampling=approx_downsampling, threshold=threshold)
         self.data = utils.generate_background_mask(parent_wsi, self.level)
 
     def add_patchsize(self, x):
         self.patchsize = x
+
+###
+
+# class SeedObject(object):
+#
+#     def __init__(self, wsi, npBackground, annotation, level0):
+#         mask = npBackground.data
+#         nonzero = np.nonzero(mask)
+#         factor = wsi.level_downsamples[npBackground.level]
+#         N = nonzero[0].shape[0]
+#         coordinates = [(int(nonzero[0][i] * factor), int(nonzero[1][i] * factor)) for i in range(N)]
+#         shuffle(coordinates)
+#         self.class_list = [0]
+#         self.class_seeds = [coordinates]
+#
+#         if annotation is None:
+#             return
+#
+#         down = int(level0 / 2.5)
+#         level = utils.get_level(annotation, desired_downsampling=down, threshold=20)
+#         annotation_low_res = annotation.read_region((0, 0), level, annotation.level_dimensions[level])
+#         annotation_low_res = annotation_low_res.convert('L')
+#         annotation_low_res = np.asarray(annotation_low_res).copy()
+#         classes = sorted(list(np.unique(annotation_low_res)))
+#         assert classes[0] == 0
+#         classes = classes[1:]
+#         for c in classes:
+#             mask = (annotation_low_res == c)
+#             nonzero = np.nonzero(mask)
+#             factor = annotation.level_downsamples[level]
+#             N = nonzero[0].shape[0]
+#             coordinates = [(int(nonzero[0][i] * factor), int(nonzero[1][i] * factor)) for i in range(N)]
+#             shuffle(coordinates)
+#             self.class_list.append(c)
+#             self.class_seeds.append(coordinates)
--- test.py
+++ test.py
@@ -1,36 +1,54 @@
 import os
 import pickle
 import time
+import glob
+
+from joblib import Parallel, delayed
+import multiprocessing
+
 from modules import single_sampler
 
 ###
 
 data_dir = '/home/peter/Dropbox/publish-final/WSI_sampler_data'
 
-file = os.path.join(data_dir, 'Tumor_004.tif')
+###
+
+downsampling = 4.
+size = 256
+per_class = 100
+
+###
+
+files = glob.glob(os.path.join(data_dir, '*.tif'))
+print('Found files {}'.format(files))
+
 bg_dir = os.path.join(data_dir, 'background')
 mask_dir = os.path.join(data_dir, 'annotation')
 
+
+def do_file(file):
+    sampler = single_sampler.Single_Sampler(wsi_file=file, background_dir=bg_dir, annotation_dir=mask_dir, level0=40.)
+    sampler.prepare_sampling(desired_downsampling=downsampling, patchsize=size)
+    sampler.sample_patches(max_per_class=per_class, savedir='./patchframes')
+
+start=time.time()
+for file in files:
+    do_file(file)
+print('Serial time'.format(time.time()-start))
+
 ###
 
-sampler = single_sampler.Single_Sampler(wsi_file=file, background_dir=bg_dir, annotation_dir=mask_dir, level0=40)
+num_cores = multiprocessing.cpu_count()
+
+start=time.time()
+Parallel(n_jobs=num_cores)(delayed(do_file)(file) for file in files)
+print('Parallel time '.format(time.time()-start))
 
 ###
 
-sampler.prepare_sampling(desired_downsampling=4, patchsize=256)
-
-# patch, info = sampler.get_random_patch()
-# patch.show()
-# print(info)
-
-# patch, info = sampler.get_class0_patch()
-# patch.show()
-# print(info)
-
-sampler.sample_patches(max_per_class=5, savedir=os.getcwd())
-
-pickling_off = open('./Tumor_004_patchframe.pickle', 'rb')
-frame = pickle.load(pickling_off)
-pickling_off.close()
-
-print(frame.head(n=10))
+# pickling_off = open('./Tumor_004_patchframe.pickle', 'rb')
+# frame = pickle.load(pickling_off)
+# pickling_off.close()
+#
+# print(frame.head(n=10))
